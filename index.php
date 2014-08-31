6a56b64bc854254c431b365e5227d44daa74e303
rafamalaga86 <rafamalaga86@gmail.com>
2014-09-01 00:21:44 +0100

    Portfolio morphing

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <!-- Hey! Thank you for taking some time to look the code, hope you like what you see. If you want to ask anything, please contact me. Rafael. -->
-<?php 
-    include 'functions.php';
- ?>
-
 <!doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Rafael García Doblas</title>
-        <meta name="description" content="Rafael García Doblas, front-end developer">
+        <meta name="description" content="Rafael García Doblas, Web Developer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, minimal-ui">
         <meta name="format-detection" content="telephone=no">
 
@@ -19,135 +15,162 @@
         <!-- LESS JS -->
         <script>var less = { env:"development", dumpLineNumbers:'mediaquery'};</script>
         <script src="/public/js/less-1.7.4.min.js"></script>
-        
     </head>
 
     <body>
-
-        <section class="s-first full-screen-section">
-            <div class="floating-box">
-                <h3><?= random_greetings(); ?></h3>
-                <h1>I am Rafael García Doblas</h1>
-                <h2>A proud web developer. I love noodle soup, motorbikes, and travelling.</h2>
-            </div>
-        </section>
-
-        <section class="s-greetings">
-            <div class="long-hold">
-                <div class="first">
-                    <h2>This is what I do</h2>
-                </div><!--
-            i--><div class="second">
-                    <p>Love websites. To plan them, to study every detail, to work along my friend designers, to discuss about UX...</p>
-                    <p>Love Sublime Text and like to keep up to date in website techonology</p>
+        <div class="wrapper">
+            <section class="s-first full-screen-section">
+                <div class="floating-box">
+                    <h3>Hi <span class="red">There!</span></h3>
+                    <h1>I am <span class="red">Rafael García Doblas</span></h1>
+                    <p>A proud <span class="red">web developer</span>. I love <span class="curly-brackets">{{</span> <span class="red like"> motorbikes </span> <span class="curly-brackets">}}</span></p>
                 </div>
-                <a href="#" class="button">Download my CV</a>
-            </div>
-        </section>
+            </section>
 
-        <section class="s-portfolio">
-            <ul>
-                <li class="box unit vertical box-4">
-                    <div class="inner">
-                        <div class="inside-inner">
-                            <h5>Wonderlabs Website</h5>
-                            <a href="#" class="button">See more</a>
-                        </div>
+            <section class="s-greetings">
+                <div class="hold">
+                    <div class="first">
+                        <h2>This is what I do</h2>
+                    </div><!--
+                i--><div class="second">
+                        <p>Love websites. To plan them, to study every detail, to work along my friend designers, to discuss about UX...</p>
+                        <p>Love Sublime Text and like to keep up to date in website techonology</p>
                     </div>
-
-                <li class="box unit vertical box-5">
-                    <div class="inner">
-                        <div class="inside-inner">
-                            <h5>MyRedLights Website</h5>
-                            <a href="#" class="button">See more</a>
-                        </div>
-                    </div>
-            </ul><!--
-        i--><ul>
-                <li class="box unit box-1">
-                    <div class="inner">
-                        <div class="inside-inner">
-                            <h5>Clickcreacion Website</h5>
-                            <a href="#" class="button">See more</a>
-                        </div>
-                    </div>
-
-                <li class="box unit box-2">
-                    <div class="inner">
-                        <div class="inside-inner">
-                            <h5>Hidromainake Website</h5>
-                            <a href="#" class="button">See more</a>
-                        </div>
-                    </div>
-
-                <li class="box unit horizontal box-3">
-                    <div class="inner">
-                        <div class="inside-inner">
-                            <h5>Biberodromo Website</h5>
-                            <a href="#" class="button">See more</a>
-                        </div>
-                    </div>
-            </ul>
-        </section>
-
-
-        <section class="s-bars">
-        </section>
-
-
-        <section class="s-cta">
-            <div class="hold">
-                <div class="floating-text">
-                    <h2>This is me. Rafa.</h2>
-                    <p>I always answer my emails and text messages, but not always pick the phone because I could be at work. I would love to hear from you.</p>
-                    <a href="" class="button mail-rafa">Mail Rafa</a>
-                    <div href="" class="button call-rafa">Call Rafa</div>
+                    <a href="/public/others/Rafael%20Garcia%20Doblas%20CV.docx" class="button vertical-swap">
+                        <span class="first-content">Download my CV</span>
+                        <i class="second-content icon-signup"></i>
+                    </a>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <section class="s-download">
-            Download my CV
-        </section>
+            <section class="s-morphing">
+                <div class="md-slider">
+                    <div class="md-device-wrapper">
+                        <div class="md-device md-device-1">
+                            <a href="#"><img src="/public/images/morphing/site1.jpg"></a>
+                            <div class="md-border-element"></div>
+                            <div class="md-base-element"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-
-        <section class="s-testimonials">
-            <div class="hold">
+            <section class="s-portfolio">
                 <ul>
-                    <li>
-                        <div class="i-container"><i class="icon-quote"></i></div>
-                        <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
-                        <h5 class="name">Adam Houston</h5>
-                        <h6 class="sub-title">Bridge Creative</h6>
+                    <li class="box unit vertical box-4">
+                        <div class="inner">
+                            <div class="inside-inner">
+                                <h5>Wonderlabs Website</h5>
+                                <a href="#" class="button">See more</a>
+                            </div>
+                        </div>
 
-                    <li>
-                        <div class="i-container"><i class="icon-quote"></i></div>
-                        <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
-                        <h5 class="name">Adam Houston</h5>
-                        <h6 class="sub-title">Bridge Creative</h6>
+                    <li class="box unit vertical box-5">
+                        <div class="inner">
+                            <div class="inside-inner">
+                                <h5>MyRedLights Website</h5>
+                                <a href="#" class="button">See more</a>
+                            </div>
+                        </div>
+                </ul><!--
+            i--><ul>
+                    <li class="box unit box-1">
+                        <div class="inner">
+                            <div class="inside-inner">
+                                <h5>Clickcreacion Website</h5>
+                                <a href="#" class="button">See more</a>
+                            </div>
+                        </div>
 
-                    <li>
-                        <div class="i-container"><i class="icon-quote"></i></div>
-                        <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
-                        <h5 class="name">Adam Houston</h5>
-                        <h6 class="sub-title">Bridge Creative</h6>
+                    <li class="box unit box-2">
+                        <div class="inner">
+                            <div class="inside-inner">
+                                <h5>Hidromainake Website</h5>
+                                <a href="#" class="button">See more</a>
+                            </div>
+                        </div>
+
+                    <li class="box unit horizontal box-3">
+                        <div class="inner">
+                            <div class="inside-inner">
+                                <h5>Biberodromo Website</h5>
+                                <a href="#" class="button">See more</a>
+                            </div>
+                        </div>
                 </ul>
-            </div>
-        </section>
+            </section>
 
-        <section class="s-socia-networks">
-            <ul>
-                <li class="twitter">
-                    <i class="icon-twitter"></i>
-                <li class="linkedin">
-                    <i class="icon-linkedin"></i>
-                <li class="skype">
-                    <i class="icon-skype"></i>
-                <li class="facebook">
-                    <i class="icon-facebook"></i>
-            </ul>
-        </section>
 
+            <section class="s-bars">
+            </section>
+
+
+            <section class="s-cta">
+                <div class="hold">
+                    <div class="floating-text">
+                        <h2>This is <span>Rafa</span>.</h2>
+                        <p>I mean, it's me. I answer my emails, I answer my phone, and I enjoy helping clients to find solutions to business challenges.</p>
+                        <p class="red mt">I'd love to hear from you.</p>
+                        <div class="container">
+                            <a href="" class="button mail-rafa vertical-swap">
+                                <span class="first-content">Mail Rafa</span>
+                                <i class="second-content icon-paperplane"></i>
+                            </a>
+                            <div href="" class="button call-rafa vertical-swap">
+                                <span class="first-content">Call Rafa</span>
+                                <i class="second-content icon-phone"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="s-testimonials">
+                <div class="hold">
+                    <ul>
+                        <li>
+                            <div class="i-container"><i class="icon-quote"></i></div>
+                            <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
+                            <h5 class="name">Adam Houston</h5>
+                            <h6 class="sub-title">Bridge Creative</h6>
+
+                        <li>
+                            <div class="i-container"><i class="icon-quote"></i></div>
+                            <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
+                            <h5 class="name">Adam Houston</h5>
+                            <h6 class="sub-title">Bridge Creative</h6>
+
+                        <li>
+                            <div class="i-container"><i class="icon-quote"></i></div>
+                            <p>Working with Rafa was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.</p>
+                            <h5 class="name">Adam Houston</h5>
+                            <h6 class="sub-title">Bridge Creative</h6>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="s-socia-networks">
+                <ul>
+                    <li class="twitter">
+                        <a href="">
+                            <i class="icon-twitter"></i>
+                        </a>
+                    <li class="linkedin">
+                        <a href="https://www.linkedin.com/pub/rafael-garcia-doblas/41/691/b75">
+                            <i class="icon-linkedin"></i>
+                        </a>
+                    <li class="skype vertical-swap-opposite">
+                        <div>
+                            <i class="first-content icon-skype"></i>
+                            <span class="second-content">ID: rafamalaga86</span>
+                        </div>
+                    <li class="facebook">
+                        <a href="https://www.facebook.com/rafamalaga86">
+                            <i class="icon-facebook"></i>
+                        </a>
+                </ul>
+            </section>
+        </div>
 
         <footer class="s-form full-screen-section">
             <form id="theForm" class="simform" autocomplete="off">
@@ -181,33 +204,38 @@
             </form><!-- /simform -->            
         </footer>
 
+<!--
+<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
+<div id="SkypeButton_Call_rafamalaga86_1">
+  <script type="text/javascript">
+    Skype.ui({
+      "name": "chat",
+      "element": "SkypeButton_Call_rafamalaga86_1",
+      "participants": ["rafamalaga86"],
+      "imageSize": 32
+    });
+  </script>
+</div>
+-->
 
-        <!--<footer>
-            <div class="small-12 medium-6 large-4">A graduate of Internet Technology, Ian combines his education with 6 years commercial experience in CSS3, HTML5, JavaScript, WordPress, and design to produce creative and effective websites for happy clients over the world.
-
-Freelance Front End Developer. Available September 2014.</div>
-
-            <div class="small-12 medium-6 large-4">
-                <i></i><p>Follow @rafamalaga86 for web design & development articles, opinions and links</p>
-            </div>
-
-            <div class="small-12 medium-6 large-4">
-
-            </div>
-
-        </footer>-->
         <!-- TIME FOR SCRIPTS -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="/public/js/fastclick.js"></script>
         <script src="/public/js/foundation.min.js"></script>
 
-        <script src="/public/form/classie.js"></script>
-        <script src="/public/form/modernizr.custom.js"></script>
-        <script src="/public/form/stepsForm.js"></script>
+        <!-- form -->
+        <script src="/public/js/classie.js"></script>
+        <script src="/public/js/modernizr.custom.js"></script>
+        <script src="/public/js/stepsForm.js"></script>
+
+
+        <!-- morphing device -->
+        <script src="/public/js/morphingdevice.js"></script>
 
         <script>
             
         </script>
+
 
         <script src="/public/js/rafascript.js"></script>
     </body>
--- public/js/rafascript.js
+++ public/js/rafascript.js
@@ -1,17 +1,151 @@
+// Rafael García Doblas
+
+
+
+// Preload images
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+        // Alternatively you could use:
+        // (new Image()).src = this;
+    });
+}
+
+preload([
+    '/public/images/morphing/site1.jpg',
+    '/public/images/morphing/site2.jpg',
+    '/public/images/morphing/site3.jpg',
+    '/public/images/morphing/site4.jpg',
+    '/public/images/morphing/site3r.jpg',
+    '/public/images/morphing/site4r.jpg'
+    ]);
+
+// MORPHING SECTION
+////////////////////////////////////////////////////////////////////////
+
+
+
+var el = document.querySelector( '.md-slider' ),
+    settings = {
+        autoplay : true,
+        interval : 3000,
+        devices : [
+            { cName : 'md-device-1', canRotate : false, imgsrc : '/public/images/morphing/site1.jpg' },
+            { cName : 'md-device-2', canRotate : false, imgsrc : '/public/images/morphing/site2.jpg' },
+            { cName : 'md-device-3', canRotate : true, imgsrc : '/public/images/morphing/site3.jpg', rotatedsrc : '/public/images/morphing/site3r.jpg' },
+            { cName : 'md-device-4', canRotate : true, imgsrc : '/public/images/morphing/site4.jpg', rotatedsrc : '/public/images/morphing/site4r.jpg' }
+        ]
+    },
+    devicesTotal = settings.devices.length,
+    ds = MorphingDevice( el, settings );
+
+// create navigation triggers
+var nav = document.createElement( 'nav' );
+for( var i = 0; i < devicesTotal; ++i ) {
+
+    var trigger = document.createElement( 'a' );
+    trigger.className = i === 0 ? 'md-current' : '';
+    trigger.setAttribute( 'href', '#' );
+    trigger.setAttribute( 'pos', i );
+    trigger.onclick = function( event ) {
+
+        ds.stopSlideshow();
+        var pos = Number( event.target.getAttribute( 'pos' ) );
+        if( pos === ds.getCurrent() ) {
+            return false;
+        }
+        ds.updateCurrentTrigger( event.target );
+        ds.setCurrent( pos );
+        ds.changeDevice();
+        return false;
+    };
+    nav.appendChild( trigger );
+
+}
+el.appendChild( nav );
+
+if( settings.autoplay ) {
+    ds.startSlideshow();
+}
+
+
+
+
+
+
+
+el = document.querySelector( '.md-slider' );
+settings = {
+    autoplay : true,
+    interval : 3000,
+    devices : [
+        { cName : 'md-device-1', canRotate : false, imgsrc : '/public/images/morphing/site1-2.jpg' },
+        { cName : 'md-device-2', canRotate : false, imgsrc : '/public/images/morphing/site2-2.jpg' },
+        { cName : 'md-device-3', canRotate : true, imgsrc : '/public/images/morphing/site3-2.jpg', rotatedsrc : '/public/images/morphing/site3r.jpg' },
+        { cName : 'md-device-4', canRotate : true, imgsrc : '/public/images/morphing/site4-2.jpg', rotatedsrc : '/public/images/morphing/site4r.jpg' }
+    ]
+};
+devicesTotal = settings.devices.length;
+ds = MorphingDevice( el, settings );
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+        // jQuery
+////////////////////////////////////////////////////////////////////////////////////////
 jQuery(function($){
 
 winh = $(window).height();
 
 
 $(".full-screen-section").height(winh);
+$('body').css("padding-bottom", winh);
 
 
 $(window).on('resize', function(){ // ON WINDOWS RESIZE
 	winh = $(window).height();
 
 	$(".full-screen-section").height(winh);
+    $('body').css("padding-bottom", winh);
 });
 
+
+
+// FIRST SECTION 
+///////////////////////////////////////////////////////////////////
+
+var array_likes = [
+    "travelling",
+    "sublime text",
+    "noodle soup",
+    "motorbikes"
+];
+var array_likes_count;
+
+function rounded_array_count(){
+    array_likes_count++;
+    // console.log(array_likes_count + " > " + array_likes.length);
+    if (array_likes_count >= array_likes.length){
+        array_likes_count = 0;
+    }
+}
+
+var random = Math.random() * array_likes.length;
+array_likes_count = Math.round(random);
+
+var like = document.getElementsByClassName('like');
+like[0].innerHTML = array_likes[array_likes_count];
+
+setInterval(function(){
+    $(".like").fadeOut('fast', function(){
+        rounded_array_count();
+        var what_i_like = array_likes[array_likes_count];
+        $('.like').html(what_i_like);
+        $('.like').fadeIn("fast");
+    });
+}, 4000);
 
 
 
