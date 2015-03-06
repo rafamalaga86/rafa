6c06299969e6cd5f5f06e3e3bd1e02b09141809c
rafamalaga86 <rafamalaga86@gmail.com>
2015-03-06 13:37:11 +0100

    Tracking snippets

--- index.php
+++ index.php
@@ -504,6 +504,36 @@
 
         <!-- <script src="/public/js/rafascript.js"></script> -->
 
+
+
+        <!-- KISS tracking snippet -->
+        <script type="text/javascript">var _kmq = _kmq || [];
+            var _kmk = _kmk || '1a4b6393e13fbdfce0bdd4f2ac228748436cf895';
+            function _kms(u){
+              setTimeout(function(){
+                var d = document, f = d.getElementsByTagName('script')[0],
+                s = d.createElement('script');
+                s.type = 'text/javascript'; s.async = true; s.src = u;
+                f.parentNode.insertBefore(s, f);
+              }, 1);
+            }
+            _kms('//i.kissmetrics.com/i.js');
+            _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+        </script>
+
+        <!-- Analytics -->
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-60450670-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+
        
 
     </body> 
